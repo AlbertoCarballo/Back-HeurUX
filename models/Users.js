80208a9cde79ed04abbe4afc9b98d50f02c7ee92
AlbertoCarballo <161271994+AlbertoCarballo@users.noreply.github.com>
2025-10-12 23:55:35 -0700

    se añaden campos al model para la gestion de los datos

--- models/Users.js
+++ models/Users.js
@@ -1,28 +1,51 @@
 import mongoose from "mongoose";
 
 const respuestaSchema = new mongoose.Schema({
-    seccion1: [Number],
-    seccion2: [Number],
-    resultados: [Number] // ← nuevo arreglo para almacenar resultados finales
+    section1: [Number],
+    section2: [Number],
+    section3: [Number],
+    section4: [Number],
+    section5: [Number],
+    section6: [Number],
+    section7: [Number],
+    section8: [Number],
+    section9: [Number],
+    section10: [Number],
+    section11: [Number],
+    section12: [Number],
+    section13: [Number],
+    section14: [Number],
+    section15: [Number],
+    results: [Number],
+    average: { type: Number }
 }, { _id: false });
 
 const testerSchema = new mongoose.Schema({
-    id: { type: String, required: true }, // correo del tester
-    respuestas: [respuestaSchema]
+    id: { type: String, required: true },
+    answers: [respuestaSchema]
 }, { _id: false });
 
 const projectSchema = new mongoose.Schema({
     id: { type: String, required: true },
-    nombre: { type: String, required: true },
+    name: { type: String, required: true },
+    URL: {type: String, required: true},
+    description: {type: String, required: true},
+    complexity: {type: String, requided: true},
     tester: [testerSchema]
 }, { _id: false });
 
 const userSchema = new mongoose.Schema({
-    id: { type: String, required: true }, // correo del usuario
-    nombre: { type: String, required: true },
-    testing: [String], // IDs de proyectos que el usuario está probando
-    friends: [String], // correos de amigos
-    projects: [projectSchema] // lista de proyectos
+    id: { type: String, required: true }, // correo
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    dob: { type: Date },
+    experiences: { type: [String], default: [] },
+    languages: { type: [String], default: [] },
+    phone: { type: [String], default: [] },
+    grades: { type: [String], default: [] },
+    testing: { type: [String], default: [] },
+    friends: { type: [String], default: [] },
+    projects: { type: [projectSchema], default: [] }
 }, { timestamps: true });
 
 export default mongoose.model("User", userSchema);
